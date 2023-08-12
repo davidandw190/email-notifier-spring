151da33c7cb68c0ac231efdc2a90a929bf911a0a
Andrei-David Nan <nandavid190@gmail.com>
2023-08-12 12:18:26 +0300

    v0.1 - Initial commit;

---
+++ src/main/java/com/davidandw190/emailnotifier/EmailNotifierSpringApplication.java
@@ -0,0 +1,13 @@
+package com.davidandw190.emailnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotifierSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailNotifierSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidandw190/emailnotifier/EmailNotifierSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidandw190.emailnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotifierSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
